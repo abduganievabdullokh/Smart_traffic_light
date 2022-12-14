9f9a1841296637292a2fdecde4b0c0b0a570293e
Abdullokh <99742107+abduganievabdullokh@users.noreply.github.com>
2022-12-14 23:42:06 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Light{
+private:
+    int carNumber;
+    string color;
+    double time;
+public:
+    void setCarNumber(int CarNumber){
+        carNumber = CarNumber;
+    }
+    int getCarNumber(){
+        return carNumber;
+    }
+    void setColor(string Color){
+        color = Color;
+    }
+    string getColor(){
+        return color;
+    }
+    void GreenLight(int CarNumber){
+        if(carNumber>=8){
+            color = "green";
+        }
+    }
+    void RedLight(int CarNumber){
+        if(carNumber<=7){
+            color = "red";
+        }
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
